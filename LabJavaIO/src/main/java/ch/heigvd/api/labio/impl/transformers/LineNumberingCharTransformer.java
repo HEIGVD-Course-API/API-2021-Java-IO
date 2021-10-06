d7f75bf6250865e944901e6b370b726747ab6cba
AJaquier <alex.jaquier@gmail.com>
2021-10-06 11:46:51 +0200

    feat: 5 - lineNumberCharTransformer

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
@@ -1,5 +1,6 @@
 package ch.heigvd.api.labio.impl.transformers;
 
+import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
@@ -18,10 +19,19 @@ import java.util.logging.Logger;
  */
 public class LineNumberingCharTransformer {
   private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+  private int number = 0;
 
   public String transform(String c) {
-    /* TODO: implement the transformation here.
-     */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String st = "";
+    if (number == 0) {
+      st = ++number + ". ";
+    }
+    if (Objects.equals(c, "\r")){
+      return "";
+    } else if (Objects.equals(c, "\n")){
+      return st += "\n"+ ++number + ". ";
+    }
+    return st += c;
+
   }
 }
