5ed05dad0b84fa2678ad509b8253b861932108b7
AJaquier <alex.jaquier@gmail.com>
2021-10-06 12:00:46 +0200

    feat: 5 - FileExplorer

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
@@ -23,7 +23,13 @@ public class FileExplorer {
          *  For each file, call the FileTransformer (see above).
          *  For each directory, recursively explore the directory.
          */
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
-
+        File[] filesPaths = rootDirectory.listFiles();
+        for(File file : filesPaths){
+            if (file.isFile()){
+                transformer.transform(file);
+            } else if(file.isDirectory()){
+                explore(file);
+            }
+        }
     }
 }
