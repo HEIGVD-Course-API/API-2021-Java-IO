6543e1aeab9b3f18d1396c2a678e81a5b7e93c99
Melissa Gehring <melissa.gehring@epfl.ch>
2021-10-08 18:04:00 +0200

    Add the implementation of the three Transformers

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
@@ -4,24 +4,34 @@ import java.util.logging.Logger;
 
 /**
  * This class applies a transformation to the input character (a string with a single character):
- *   1. Convert all line endings to Unix-style line endings, i.e. only '\n'.
- *   2. Add a line number at the beginning of each line.
+ * 1. Convert all line endings to Unix-style line endings, i.e. only '\n'.
+ * 2. Add a line number at the beginning of each line.
  * Example:
- *   Using this character transformer, the following file :
- *      This is the first line.\r\n
- *      This is the second line.
- *   will be transformed to:
- *      1. This is the first line.\n
- *      2. This is the second line.
+ * Using this character transformer, the following file :
+ * This is the first line.\r\n
+ * This is the second line.
+ * will be transformed to:
+ * 1. This is the first line.\n
+ * 2. This is the second line.
  *
- * @author Olivier Liechti, Juergen Ehrensberger
+ * @author Olivier Liechti, Juergen Ehrensberger, Melissa Gehring
  */
 public class LineNumberingCharTransformer {
-  private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+    private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+    private int lines = 1;
 
-  public String transform(String c) {
-    /* TODO: implement the transformation here.
-     */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public String transform(String c) {
+        String s = "";
+        if (lines == 1) {
+            s = lines++ + ". ";
+        }
+        switch (c) {
+            case "\r":
+                return s + "";
+            case "\n":
+                return s + "\n" + lines++ + ". ";
+            default:
+                return s + c;
+        }
+    }
 }
--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/NoOpCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/NoOpCharTransformer.java
@@ -6,14 +6,12 @@ import java.util.logging.Logger;
  * This class applies no transformation to the input character (a string with a single character).
  * It is just a simple case, before implementing other character transformers.
  *
- * @author Olivier Liechti, Juergen Ehrensberger
+ * @author Olivier Liechti, Juergen Ehrensberger, Melissa Gehring
  */
 public class NoOpCharTransformer {
-  private static final Logger LOG = Logger.getLogger(NoOpCharTransformer.class.getName());
+    private static final Logger LOG = Logger.getLogger(NoOpCharTransformer.class.getName());
 
-  public String transform(String c) {
-    /* TODO: implement the transformation here.
-     */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public String transform(String c) {
+        return c;
+    }
 }
