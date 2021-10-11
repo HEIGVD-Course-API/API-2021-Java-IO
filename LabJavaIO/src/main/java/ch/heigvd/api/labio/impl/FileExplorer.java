ba08aa8810b5abd86129288b744f4c0b8506d036
Maelle <maelle.vogel@heig-vd.ch>
2021-10-11 14:45:09 +0200

    Test passed, recursive search

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
@@ -2,6 +2,8 @@ package ch.heigvd.api.labio.impl;
 
 import java.io.File;
 
+import static java.util.Arrays.sort;
+
 /**
  * The FileExplorer performs an exploration of the file system. It
  * visits the files and directory in alphabetic order.
@@ -13,9 +15,8 @@ import java.io.File;
 public class FileExplorer {
 
     public void explore(File rootDirectory) {
-        FileTransformer transformer = new FileTransformer();
 
-        /* TODO: implement the logic to explore the rootDirectory.
+        /* implement the logic to explore the rootDirectory.
          *  Use the Java JDK documentation to see:
          *  - how to get the files and directories of rootDirectory (which is of class File)
          *  - to sort the items (files and directories) alphabetically
@@ -23,7 +24,24 @@ public class FileExplorer {
          *  For each file, call the FileTransformer (see above).
          *  For each directory, recursively explore the directory.
          */
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+        if(rootDirectory.exists()){
+            if(rootDirectory.isDirectory()){
+                String[] file = rootDirectory.list();
+
+                assert file != null;
+                sort(file);
+
+                for(String doc : file){
+                    explore(new File(rootDirectory.getPath() + "/"+ doc));
+                }
+            }else if (rootDirectory.isFile()){
+                FileTransformer transformer = new FileTransformer();
+                transformer.transform(rootDirectory);
+            }
+
+        }
+
 
     }
 }
