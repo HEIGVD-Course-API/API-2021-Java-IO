8198aa84a681a3e2cc0c8d19f57e5c7243950978
Alice Grunder <alice@nerdgautica.net>
2021-10-13 09:58:48 +0200

    UpperCaseCharTransformer works

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/UpperCaseCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/UpperCaseCharTransformer.java
@@ -1,5 +1,6 @@
 package ch.heigvd.api.labio.impl.transformers;
 
+import java.util.Locale;
 import java.util.logging.Logger;
 
 /**
@@ -14,6 +15,8 @@ public class UpperCaseCharTransformer {
   public String transform(String c) {
     /* TODO: implement the transformation here.
      */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    return c.toUpperCase(Locale.ROOT);
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
 }
