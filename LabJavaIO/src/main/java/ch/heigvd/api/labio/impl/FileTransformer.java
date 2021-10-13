f7aae465e05688b388d25eae73867cd9a1cee3bc
Géraud Silvestri <silvestri.geraud@gmail.com>
2021-10-13 11:05:37 +0200

    All tests passed

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileTransformer.java
@@ -1,6 +1,10 @@
 package ch.heigvd.api.labio.impl;
+import ch.heigvd.api.labio.impl.transformers.UpperCaseCharTransformer;
+import ch.heigvd.api.labio.impl.transformers.LineNumberingCharTransformer;
+import ch.heigvd.api.labio.impl.transformers.NoOpCharTransformer;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,32 +19,25 @@ public class FileTransformer {
   private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
 
   public void transform(File inputFile) {
-    /*
-     * This method opens the given inputFile and copies the
-     * content to an output file.
-     * The output file has a file name <inputFile-Name>.out, for example:
-     *   quote-2.utf --> quote-2.utf.out
-     * Both files must be opened (read or write) with encoding "UTF-8".
-     * Before writing each character to the output file, the transformer calls
-     * a character transformer to transform the character before writing it to the output.
-     */
-
-    /* TODO: first start with the NoOpCharTransformer which does nothing.
-     *  Later, replace it by a combination of the UpperCaseCharTransformer
-     *  and the LineNumberCharTransformer.
-     */
-    // ... transformer = ...
-
-    /* TODO: implement the following logic here:
-     *  - open the inputFile and an outputFile
-     *    Use UTF-8 encoding for both.
-     *    Filename of the output file: <inputFile-Name>.out (that is add ".out" at the end)
-     *  - Copy all characters from the input file to the output file.
-     *  - For each character, apply a transformation: start with NoOpCharTransformer,
-     *    then later replace it with a combination of UpperCaseFCharTransformer and LineNumberCharTransformer.
-     */
     try {
+      InputStreamReader isr = new InputStreamReader(new FileInputStream(inputFile),
+              StandardCharsets.UTF_8);
+      File output = new File(inputFile.getParent(),inputFile.getName()+".out");
+      OutputStreamWriter owr = new OutputStreamWriter(new FileOutputStream(output),
+              StandardCharsets.UTF_8);
 
+      LineNumberingCharTransformer transformer = new LineNumberingCharTransformer();
+      UpperCaseCharTransformer upper = new UpperCaseCharTransformer();
+
+      int r;
+
+      while((r = isr.read()) != -1){
+        owr.write(transformer.transform(upper.transform(Character.toString(r))));
+      }
+
+      isr.close();
+      owr.flush();
+      owr.close();
     } catch (Exception ex) {
       LOG.log(Level.SEVERE, "Error while reading, writing or transforming file.", ex);
     }
