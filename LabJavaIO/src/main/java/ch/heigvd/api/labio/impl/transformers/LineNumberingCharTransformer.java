2eb37cf72b9fc405082bb57d1768637205f7183c
Eliott <f>
2021-10-13 09:19:47 +0200

    Class LineNumberingCharTransformer done

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
@@ -18,10 +18,24 @@ import java.util.logging.Logger;
  */
 public class LineNumberingCharTransformer {
   private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+  private int lineCounter = 0;
 
   public String transform(String c) {
     /* TODO: implement the transformation here.
      */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    if (c.equals("\r")) {
+      c = "";
+    }
+
+    String cCopy = c;
+    if (lineCounter == 0) {
+      c = ++lineCounter + ". " + c;
+    }
+    if (cCopy.equals("\n")) {
+      c += ++lineCounter + ". ";
+    }
+
+    return c;
   }
 }
