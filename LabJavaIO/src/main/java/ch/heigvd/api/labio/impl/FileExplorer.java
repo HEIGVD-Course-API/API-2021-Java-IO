ee39e2d05e51a0ec05fcd266d3af6174f985f394
antoineuldry <antoine.uldry@heig-vd.ch>
2021-10-18 23:45:30 +0200

    FileExplorer v1

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/FileExplorer.java
@@ -1,6 +1,7 @@
 package ch.heigvd.api.labio.impl;
 
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * The FileExplorer performs an exploration of the file system. It
@@ -15,17 +16,24 @@ public class FileExplorer {
     public void explore(File rootDirectory) {
         FileTransformer transformer = new FileTransformer();
 
-        /* TODO: implement the logic to explore the rootDirectory.
-         *  Use the Java JDK documentation to see:
-         *  - how to get the files and directories of rootDirectory (which is of class File)
-         *  - to sort the items (files and directories) alphabetically
-         *  - to check if an item is a file or a directory
-         *  For each file, call the FileTransformer (see above).
-         *  For each directory, recursively explore the directory.
-         */
+        // Check if rootDirectory exists as Directory
+        if(rootDirectory.exists()) {
 
+            // If it's a File --> transform()
+            if (rootDirectory.isFile()) transformer.transform(rootDirectory);
 
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+            // If not --> Explore rootDirectory
+            File[] contents = rootDirectory.listFiles();
 
+            // Check if contents is not empty (null) and sort alphabetically as default
+            if(contents != null) {
+                Arrays.sort(contents);
+
+                // If it's a Directory --> explore()
+                for ( File f : contents) {
+                    explore(f);
+                }
+            }
+        }
     }
 }
