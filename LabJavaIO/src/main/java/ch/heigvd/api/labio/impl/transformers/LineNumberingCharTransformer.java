d4bf228e839b9e44bd3e50cc067332b93687eff2
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2022-03-08 20:29:33 +0100

    [TASK] Implemented transformers

--- LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/api/labio/impl/transformers/LineNumberingCharTransformer.java
@@ -4,24 +4,36 @@ import java.util.logging.Logger;
 
 /**
  * This class applies a transformation to the input character (a string with a single character):
- *   1. Convert all line endings to Unix-style line endings, i.e. only '\n'.
- *   2. Add a line number at the beginning of each line.
+ * 1. Convert all line endings to Unix-style line endings, i.e. only '\n'.
+ * 2. Add a line number at the beginning of each line.
  * Example:
- *   Using this character transformer, the following file :
- *      This is the first line.\r\n
- *      This is the second line.
- *   will be transformed to:
- *      1. This is the first line.\n
- *      2. This is the second line.
+ * Using this character transformer, the following file :
+ * This is the first line.\r\n
+ * This is the second line.
+ * will be transformed to:
+ * 1. This is the first line.\n
+ * 2. This is the second line.
  *
  * @author Olivier Liechti, Juergen Ehrensberger
  */
 public class LineNumberingCharTransformer {
-  private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+    private static final Logger LOG = Logger.getLogger(UpperCaseCharTransformer.class.getName());
+    private boolean newLine = true;
+    private int currentLine = 1;
 
-  public String transform(String c) {
-    /* TODO: implement the transformation here.
-     */
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public String transform(String c) {
+        if (newLine) {
+            // This handles blank lines (ignoring them)
+            if (c.equalsIgnoreCase("\n") || c.equalsIgnoreCase("\r"))
+                return "";
+
+            newLine = false;
+            return String.format("%d. %s", currentLine++, c);
+        } else if (c.equalsIgnoreCase("\r")) {
+            return "";
+        } else if (c.equalsIgnoreCase("\n")) {
+            newLine = true;
+        }
+        return c;
+    }
 }
